9b3d2ed1479d4178b24802077da922401803b952
Hamza Boussairi <bouh2028@usherbrooke.ca>
2023-12-10 20:35:49 -0500

    notebook_v5

--- projet_ift712/src/Preprocess_Data.py
+++ projet_ift712/src/Preprocess_Data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 
 class Preprocess_Data: 
     def __init__(self, path): 
@@ -28,4 +29,11 @@ class Preprocess_Data:
         targets = df[target]
         X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size = 0.33, random_state=42, stratify = targets)
         return X_train, X_test, y_train, y_test
+
+    def PCA(self, data , n_components):
+        pca = PCA(n_components=n_components)
+        principalComponents = pca.fit_transform(data)
+        principalDf = pd.DataFrame(data = principalComponents, columns=[f"component_{i+1}" for i in range(n_components)])
+        finalDf = pd.concat([principalDf, data['species']], axis = 1)
+        return finalDf
     
