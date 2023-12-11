87e9f491dbc97a141ed0d1978cb615551bb6a7fb
jinane <94868092+jinane-boufaris@users.noreply.github.com>
2023-12-11 14:29:02 -0500

    Comment Classify_data file

--- projet_ift712/src/Classify_Data.py
+++ projet_ift712/src/Classify_Data.py
@@ -5,17 +5,32 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, accur
 
 class Classify_Data:  
     def __init__(self): 
+        """
+        Constructor for the Classify_Data class
+        """
+        #Get all the classifiers that are available in sklearn library
         self.estimators = all_estimators(type_filter="classifier")
 
     def get_classifiers(self, six_classifiers): 
+        """
+        Creates an instance of each classifier with hyperparameters
+
+        Args: 
+            six_classifiers: List of dictionnaries that defines each classifier
+
+        Returns:
+            sk_classifiers: Same as output list but with a new key,in each dictionnary,
+                            that has as value the classifier's instance
+        """
         sk_classifiers = [] 
+     
         for clf in six_classifiers:
+            """
+            loop on all the classifiers dictionnaries provided and use estimator_class to get the instance of the model
+            """
             for estimator_name, estimator_class in self.estimators:  
                 if estimator_name == clf['model']: 
-                    #create an instance of the classifier with hyperparameters
                     estimator = estimator_class()
-                    #estimator = estimator_class
-                    #sk_classifiers.append({clf['Classifier']: estimator})
                     clf ['classifier'] = estimator
                     sk_classifiers.append(clf)
                     
@@ -23,30 +38,52 @@ class Classify_Data:
         #return clf
     
     def train_classifier(self, X_train, y_train, classifiers, metric = "accuracy", fold_number=5): 
+        """
+        Train the six classifiers, using GridSearchCV to optimize the hyperparameters
+
+        Args: 
+            X_train: features used to train the model
+            y_train: labels used to train the model
+            classifiers: list of classifiers (same structure as the output of the get_classifiers fucntion)
+            metric: metric on which will be based the gridsearchcv,(we choosed accuracy since we have balanced classes)
+            fold_number: the number of folds of he cross validatonin the GridSearchCV
+
+        Returns: 
+            trained_classifiers: trained classifiers after the gridsearch
+            best_scores: list of best accuracy of each model
+            best_estimators: List of the instances of each model with the best hyperparameters
+        """
+
         trained_calssifiers = []
         best_estimators = []
         best_scores = []
+        
         for classifier_dict in classifiers:
-            #classifier = list(classifier_dict.keys())[0]
-            #hyperparmeters = classifier_dict[classifier]
             gridsearch = GridSearchCV(estimator = classifier_dict['classifier'],
                                     param_grid = classifier_dict['params'],
                                     scoring = metric,
                                     cv = fold_number)
+            
             gridsearch.fit(X_train, y_train)
+
             trained_calssifiers.append(gridsearch)
             best_scores.append(gridsearch.best_score_)
             best_estimators.append(gridsearch.best_estimator_)
+
         return trained_calssifiers, best_scores, best_estimators
 
-    '''def predict(self, X_test, trained_classifiers): 
-        all_predictions = []
-        for trained_classifier in trained_classifiers:
-            predictions = trained_classifier.predict(X_test)
-            all_predictions.append(predictions)
-        return all_predictions'''
     
     def calculate_metrics(self, y_test, y_pred):
+        """
+        Calculates four metrics 
+
+        Args: 
+            y_test: reel data labels
+            y_pred: predicions by the model
+
+        Returns: 
+            metrics: dictionnary containing the four metrics
+        """
         metrics = {}
 
         metrics['accuracy'] = accuracy_score(y_test, y_pred)
@@ -58,6 +95,18 @@ class Classify_Data:
 
 
     def predict_and_evaluate(self, X_test, y_test, trained_calssifiers, six_classifiers): 
+        """
+        Predict on test data
+
+        Args:  
+            X_test: features of test data 
+            y_test: reel labels
+            trained classifiers: List of the classifiers (output of the train_classifier function)
+            six_classifiers: List of our six classifiers (In our case they're six, could be more)
+
+        Returns: 
+            evaluation_results: dictionnary that assciate each classifier to its calculated metrics
+        """
         evaluation_results = {}
 
         for classifier , trained_classifier in zip(six_classifiers, trained_calssifiers): 
@@ -68,9 +117,4 @@ class Classify_Data:
             evaluation_results[classifier_name] = metrics
         
         return evaluation_results
-    
-    def cross_validation(self, X, y, folds, classifier, metric): 
-        scores = cross_val_score(classifier, X, y, cv = folds, scoring= metric)
-        mean_accuracy = scores.mean()
-        return mean_accuracy
     
